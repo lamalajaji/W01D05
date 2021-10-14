c1a9fa9723240c7a1c0292d93824a67c08a00c96
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-10-14 12:24:15 +0300

    complete the task

---
+++ Task2.js
@@ -0,0 +1,28 @@
+let num1 = 23;
+let name = "Lama";
+let arr = ["Python", "SQL", "JavaScript"];
+let Lama = {
+  Lama: "lama",
+  major: "MIS",
+  bYear: 1998,
+};
+
+console.log(`Hello I'm ${name}
+I'm ${num1} `);
+console.log(arr);
+console.log(Lama);
+
+//print the type of each variabul:
+
+console.log(
+  typeof num1 + " " + typeof name + " " + typeof arr + " " + typeof Lama
+);
+
+let numArr = ["Hala", 20, "Ghala", 18, "Nouf", 10];
+// i guess it will show " opject"
+console.log(typeof numArr);
+
+//create null value
+let nulVal;
+//it will be undefine
+console.log(typeof nulVal);
---
+++ task1.js
@@ -0,0 +1,19 @@
+//  Create a variable called “language” and assign it the value “JavaScript”. Then create a variable called “score” and assign it the value “10”. Write both variables to the console.
+let language = "JavaScript";
+let score = 10;
+
+console.log(language + " " + score);
+
+//Create two variables called “length” and “width”. Assign each variable a value. Use the variables to calculate the area of a rectangle with the length and width specified (remember area = length x width). Write the following to the console: “The area of the rectangle is …” where … is the area you calculated.
+
+let height = 10;
+let width = 5;
+
+console.log(`The area of the is ${height * width}`);
+
+// Create two variables called “num1” and “num2.” Assign each variable a value. Calculate and display what the remainder is if num1 is divided by num2.
+let num1 = 15;
+let num2 = 4;
+let sum = num1 % num2;
+
+console.log(`${num1} / ${num2} remainder ${sum}`);
